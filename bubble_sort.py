3def409db575f1d333f2f120818f7ce6096ca1fd
Imran Mohammad <imranian111@gmail.com>
2024-09-01 10:26:32 +0530

    Update bubble_sort.py
    corrected the 2nd for loop to improve time complexity

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,8 +1,8 @@
 a = [2,6,4,8,7,9]
 
-for i in range(len(a)-1):
-    for j in range(i+1, len(a)):
-        if a[i]>a[j]:
-            a[i], a[j] = a[j], a[i]
+for i in range(len(a)):
+    for j in range(0, len(a)-i-1):
+        if a[j]>a[j+1]:
+            a[j], a[j+1] = a[j+1], a[j]
 
 print(a)
